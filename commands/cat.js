b3147c247ae03fc95c5e5a2c3b726a3de0856704
donkeymonkey1 <32933322+donkeymonkey1@users.noreply.github.com>
2018-03-31 19:24:56 +0000

    Update cat.js

--- commands/cat.js
+++ commands/cat.js
@@ -5,7 +5,7 @@ module.exports.run = async (client, message) => {
   const { body } = await superagent
     .get('https://random.cat/meow');
 
-  const embed = new Discord.MessageEmbed()
+  const embed = new Discord.RichEmbed()
     .setDescription(':cat: MEOW MEOW HISS SCRATCH!')
     .setImage(body.file)
     .setColor(0xD0D3D4);
