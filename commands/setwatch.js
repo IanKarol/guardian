f501fa4ed25824bc5463504375280e08c4fb556c
IanKarol <37708687+IanKarol@users.noreply.github.com>
2018-03-31 21:07:18 +0200

    Update setwatch.js

--- commands/setwatch.js
+++ commands/setwatch.js
@@ -9,12 +9,14 @@ module.exports.run = (client, message, args) => {
       .setDescription('<:dmark:426081717242298370> Name watching status!');
     message.channel.send({ embed });
   }
+  
+  else if (status.length !== 0) {
   client.user.setActivity(`${status}`, {  type: "WATCHING"});
   const embed = new Discord.RichEmbed()
     .setColor("#7289DA")
     .setDescription('<:cmark:426081678784462888> You sucessfully changed watching status');
   message.channel.send({ embed });
-};
+}};
 
 module.exports.help = {
   name: "watch"
