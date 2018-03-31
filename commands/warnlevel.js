5fdb11c67ca68a7d301db520f63c38d3f6c9ad54
IanKarol <37708687+IanKarol@users.noreply.github.com>
2018-03-31 21:56:00 +0200

    Update warnlevel.js

--- commands/warnlevel.js
+++ commands/warnlevel.js
@@ -8,7 +8,7 @@ module.exports.run = async (bot, message, args) => {
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You can't do that.");
   let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
   if(!wUser) return message.channel.send("Couldn't find user.");
-  let warnlevel = warns[wUser.id].warns;
+  let warnlevel = warns[wUser.id].warn;
 
   message.channel.send(`<@${wUser.id}> has ${warnlevel} warnings.`);
 
