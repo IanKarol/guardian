19d0b8b9a7e885dde1ca45621b5b77486067f132
IanKarol <37708687+IanKarol@users.noreply.github.com>
2018-06-10 12:56:14 +0200

    Update pvp.js

--- commands/pvp.js
+++ commands/pvp.js
@@ -2,22 +2,20 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-	let user1 = args[0];
-	let user2 = args[1];
-
-	let help = args[2];
+	let user1 = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+	let user2 = message.guild.member(message.mentions.users.second() || message.guild.members.get(args[1]));
 
 	if(!user2) return message.channel.send("Please provide who you want to fight!");
-	if(help === "help") return message.channel.send("Command usage: `!pvp <user1> <user2>`");
+	if(args[0] === "help") return message.channel.send("Command usage: `!pvp <user1> <user2>`");
 
 	let results = [`${user1} got a combo on ${user2} and now ${user2} is down on 3HP but ${user2} managed to escape!`, `${user1} got a combo and killed ${user2}`, `${user2} got a combo and killed ${user1}`, `${user2} got a combo on ${user1} and now ${user1} is down on 6HP but ${user1} managed to escape!`];
 
 	let result = Math.floor((Math.random() * results.length));
 
 	let pvpEmbed = new Discord.RichEmbed()
-	.setDescription("**PvP match has began")
+	.setDescription("**PvP match has began**")
 	.setColor("#FF0000")
-	.addField(`${user1} ⚔ ${user2}`, result);
+	.addField(`${user1.id} ⚔ ${user2.id}`, results[result]);
 
 	message.channel.send(pvpEmbed);
 
